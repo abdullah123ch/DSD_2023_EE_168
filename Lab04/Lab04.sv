0f3cb6eaf03023751cbb2e7c0003143809e5972b
abdullah123ch <145594951+abdullah123ch@users.noreply.github.com>
2025-02-18 07:51:24 +0500

    Update Lab04.sv

--- Lab04/Lab04.sv
+++ Lab04/Lab04.sv
@@ -5,8 +5,7 @@ module Lab04 (
   output logic blue,
   output logic green
 );
-  assign red = (~b[0] & ~b[1]) | (a[0] & a[1]) | (a[0] & ~b[1]) | (a[1] & b[1]) | (a[1] & ~b[0]);
-  assign blue = (~a[0] & b[0]) | (a[0] & ~b[0]) | (~a[1] & b[1]) | (a[1] & ~b[1]);
-  assign green = (~a[1] & ~a[0]) | (b[1] & b[0]) | (~a[1] & b[0]) | (~a[1] & ~b[0]) | (~a[0] & b[1]);
-
+  assign red = (~b[1] & (~b[0] | a[0])) | (a[1] & (a[0] | ~b[1] | ~b[0]));
+  assign green = (~a[1] & (b[0] | b[1] | ~a[0])) | (b[1] & (b[0] | ~a[0]));
+  assign blue = (a[1] ^ b[1]) | (a[0] ^ b[0]);
 endmodule
